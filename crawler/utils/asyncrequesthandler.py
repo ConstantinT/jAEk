99f1622fb135c37e64b42f917313fc72b0f3113b
tgianko <gpellegrino@trouge.net>
2015-11-25 17:26:03 +0100

    added missing imports

--- crawler/utils/asyncrequesthandler.py
+++ crawler/utils/asyncrequesthandler.py
@@ -14,6 +14,12 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 '''
+import logging
+from models.asyncrequeststructure import AsyncRequestStructure
+from models.parametertype import ParameterType
+from utils.utils import calculate_new_parameter_type
+
+
 
 class AsyncRequestHandler():
 
